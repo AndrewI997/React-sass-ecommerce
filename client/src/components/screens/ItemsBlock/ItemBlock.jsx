e7018ca1e21fc6a1fea207b0f74b053f99920dbf
AndrewI997 <102222540+AndrewI997@users.noreply.github.com>
2022-09-11 14:42:49 +0300

    backend_integration

--- client/src/components/screens/ItemsBlock/ItemBlock.jsx
+++ client/src/components/screens/ItemsBlock/ItemBlock.jsx
@@ -10,137 +10,17 @@ import ItemsFilterBar from '../../shared/ItemsFilterBar/ItemsFilterBar';
 const ItemBlock = ({ setModalActive, itemCardModalActive, setItemCardModalActive }) => {
 
     const [showItemState, setShowItemState] = React.useState();
+    const [itemsState, setItemsState] = React.useState([]);
 
-    const [itemsState, setItemsState] = React.useState({
-        items: [{
-            id: 1,
-            name: 'Kitchen',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '120 000',
-            imageUrl: '///'
-        },
-        {
-            id: 2,
-            name: 'Shkaf',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '40 000',
-            imageUrl: '///'
-        },
-        {
-            id: 3,
-            name: 'Kitchen',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '150 000',
-            imageUrl: '///'
-        },
-        {
-            id: 4,
-            name: 'Gorka',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '370 000',
-            imageUrl: '///'
-        },
-        {
-            id: 5,
-            name: 'Shkaf',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '80 000',
-            imageUrl: '///'
-        },
-        {
-            id: 6,
-            name: 'Kitchen',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '197 000',
-            imageUrl: '///'
-        },
-        {
-            id: 7,
-            name: 'Gorka',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '60 000',
-            imageUrl: '///'
-        },
-        {
-            id: 8,
-            name: 'Kitchen',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '120 000',
-            imageUrl: '///'
-        },
-        {
-            id: 9,
-            name: 'Shkaf',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '40 000',
-            imageUrl: '///'
-        },
-        {
-            id: 10,
-            name: 'Kitchen',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '150 000',
-            imageUrl: '///'
-        },
-        {
-            id: 11,
-            name: 'Gorka',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '370 000',
-            imageUrl: '///'
-        },
-        {
-            id: 12,
-            name: 'Shkaf',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '80 000',
-            imageUrl: '///'
-        },
-        {
-            id: 13,
-            name: 'Kitchen',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '197 000',
-            imageUrl: '///'
-        },
-        {
-            id: 14,
-            name: 'Gorka',
-            typeId: '1',
-            subTypeId: '1',
-            variationId: '1',
-            price: '60 000',
-            imageUrl: '///'
-        }]
-    })
-
-    // console.log(itemsState)
+    React.useEffect(() => {
+        fetch('http://localhost:1111/items')
+            .then((res) => {
+                return res.json()
+            })
+            .then((items) => {
+                setItemsState(items)
+            })
+    }, [])
 
     return (
 
@@ -148,7 +28,7 @@ const ItemBlock = ({ setModalActive, itemCardModalActive, setItemCardModalActive
             <ItemsFilterBar />
             <div className={styles.itemsBlockWrapp}>
                 {
-                    itemsState.items.map((obj) => (
+                    itemsState.map((obj) => (
                         <ItemCard key={obj.id} setShowItemState={setShowItemState} setItemCardModalActive={setItemCardModalActive} setModalActive={setModalActive} obj={obj} name={obj.name} price={'От' + ' ' + obj.price + 'p'} img={obj.imageUrl} />
                     ))
                 }
--- client/src/components/shared/ItemsFilterBar/ItemsFilterBar.jsx
+++ client/src/components/shared/ItemsFilterBar/ItemsFilterBar.jsx
@@ -3,16 +3,77 @@ import styles from './ItemsFilterBar.module.scss'
 
 const ItemsFilterBar = () => {
 
+    const [items, setItems] = React.useState([]);
+    const [items2, setItems2] = React.useState([]);
+    const [items3, setItems3] = React.useState([]);
+    const [items4, setItems4] = React.useState([]);
 
+    React.useEffect(() => {
+        fetch('http://localhost:1111/item-types')
+            .then((res) => {
+                return res.json()
+            })
+            .then((items) => {
+                setItems(items)
+            })
+
+        fetch('http://localhost:1111/item-sub-types')
+            .then((res) => {
+                return res.json()
+            })
+            .then((items) => {
+                setItems2(items)
+            })
+
+        fetch('http://localhost:1111/item-styles')
+            .then((res) => {
+                return res.json()
+            })
+            .then((items) => {
+                setItems3(items)
+            })
+        fetch('http://localhost:1111/item-kinds')
+            .then((res) => {
+                return res.json()
+            })
+            .then((items) => {
+                setItems4(items)
+            })
+    }, [])
+    
     return (
+        <div className={styles.filterBarWrapp} >
             <div className={styles.filterBarWrapp} >
-                <div className={styles.filterBarWrapp} >
-                    <ul>
-                        <li>Корпусная</li>
-                        <li>Каркасная</li>
-                    </ul>
-                </div>
+                <ul>
+                    {
+                        items.map((obj) => (
+                            <li key={obj.id} >{obj.name}</li>
+                        ))
+                    }
+                </ul>
+                <ul>
+                    {
+                        items2.map((obj) => (
+                            <li key={obj.id} >{obj.name}</li>
+                        ))
+                    }
+                </ul>
+                <ul>
+                    {
+                        items3.map((obj) => (
+                            <li key={obj.id} >{obj.name}</li>
+                        ))
+                    }
+                </ul>
+                <ul>
+                    {
+                        items4.map((obj) => (
+                            <li key={obj.id} >{obj.name}</li>
+                        ))
+                    }
+                </ul>
             </div>
+        </div>
     )
 }
 
